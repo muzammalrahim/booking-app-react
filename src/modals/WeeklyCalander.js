a460ee17eab1a9ef540e1b7f8055e22527173128
Zeeshan123123 <creative.joomdev@gmail.com>
2023-03-07 15:10:17 +0500

    weekly calander

--- src/modals/WeeklyCalander.js
+++ src/modals/WeeklyCalander.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from "react";
 
 // liberaries
-import DatePicker from "react-horizontal-datepicker";
+import Calendar from "react-calendar";
 
 export default function WeeklyCalander() {
 
-    
+  const [date, setDate] = useState(new Date())
+
   return (
     
-    <div className='hor-calander'>
-        
-        <DatePicker 
-        color={"#5BC0CC"}
-        />
+    <div className='weekly-calander'>
+        {/*<Calendar onChange={(val) => setDateHandler(val)} value={date} />
+           {show ? (
+            <CalanderSchd
+              timeSlots={selectedTimeSlot}
+              selDate={tempTS}
+              locationId={locationId}
+            />
+          ) : null} */}
+          <Calendar onChange={setDate} value={date}/>
+       
     </div>
   )
 }
